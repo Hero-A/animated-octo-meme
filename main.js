b4341310d4aa678a6fd956878e438733a8048de5
Hero-A <92825909+Hero-A@users.noreply.github.com>
2021-12-06 20:26:06 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 230);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('my photo.jpeg');
+}
